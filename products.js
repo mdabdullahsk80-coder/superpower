04fb7fc9b2bc2b446a4a33aca3933e6b46e08d58
mdabdullahsk80-coder <mdabdullahsk80@gmail.com>
2026-03-17 00:41:38 +0600

    Add product list for various LED bulbs

---
+++ products.js
@@ -0,0 +1,52 @@
+let products = [
+
+{
+name:"LED BULB 5W",
+price:"23-95৳",
+base:23,
+image:"ledbulb5w.jpg"
+},
+
+{
+name:"LED BULB 9W",
+price:"25-110৳",
+base:25,
+image:"ledbulb9w.jpg"
+},
+
+{
+name:"LED BULB 12W",
+price:"65-120৳",
+base:65,
+image:"ledbulb12w.jpg"
+},
+
+{
+name:"LED BULB 15W",
+price:"65-135৳",
+base:65,
+image:"ledbulb15w.jpg"
+},
+
+{
+name:"LED BULB 20W",
+price:"50-160৳",
+base:50,
+image:"ledbulb20w.jpg"
+},
+
+{
+name:"LED BULB 30W",
+price:"170৳",
+base:170,
+image:"ledbulb30w.jpg"
+},
+
+{
+name:"LED BULB 50W",
+price:"280-420৳",
+base:280,
+image:"ledbulb50w.jpg"
+}
+
+]
