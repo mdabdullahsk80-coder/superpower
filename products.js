88d1b623a120550fea35aaf688a9a54f3e97f478
mdabdullahsk80-coder <mdabdullahsk80@gmail.com>
2026-03-17 00:51:33 +0600

    Refactor product entries to a concise format

--- products.js
+++ products.js
@@ -1,52 +1,80 @@
 let products = [
 
-{
-name:"LED BULB 5W",
-price:"23-95৳",
-base:23,
-image:"ledbulb5w.jpg"
-},
+{ name:"LED BULB 5W", price:"23-95৳", base:23, image:"ledbulb5w.jpg" },
+{ name:"LED BULB 9W", price:"25-110৳", base:25, image:"ledbulb9w.jpg" },
+{ name:"LED BULB 12W", price:"65-120৳", base:65, image:"ledbulb12w.jpg" },
+{ name:"LED BULB 15W", price:"65-135৳", base:65, image:"ledbulb15w.jpg" },
+{ name:"LED BULB 20W", price:"50-160৳", base:50, image:"ledbulb20w.jpg" },
+{ name:"LED BULB 30W", price:"170৳", base:170, image:"ledbulb30w.jpg" },
+{ name:"LED BULB 50W", price:"280-420৳", base:280, image:"ledbulb50w.jpg" },
 
-{
-name:"LED BULB 9W",
-price:"25-110৳",
-base:25,
-image:"ledbulb9w.jpg"
-},
+{ name:"AC/DC LED BULB 12W", price:"350৳", base:350, image:"acdc12w.jpg" },
+{ name:"AC/DC LED BULB 15W", price:"100-550৳", base:100, image:"acdc15w.jpg" },
+{ name:"AC/DC LED BULB 18W", price:"380৳", base:380, image:"acdc18w.jpg" },
 
-{
-name:"LED BULB 12W",
-price:"65-120৳",
-base:65,
-image:"ledbulb12w.jpg"
-},
+{ name:"AC TUBE LIGHT", price:"320৳", base:320, image:"actube.jpg" },
 
-{
-name:"LED BULB 15W",
-price:"65-135৳",
-base:65,
-image:"ledbulb15w.jpg"
-},
+{ name:"RGB FITA LIGHT", price:"180৳", base:180, image:"rgbfita.jpg" },
+{ name:"LED DIM LIGHT", price:"18-26৳", base:18, image:"dimlight.jpg" },
+{ name:"FAN BLADE LED BULB", price:"160৳", base:160, image:"fanblade.jpg" },
+{ name:"FAN LED BULB", price:"155৳", base:155, image:"fanled.jpg" },
 
-{
-name:"LED BULB 20W",
-price:"50-160৳",
-base:50,
-image:"ledbulb20w.jpg"
-},
+{ name:"LED DJ BULB", price:"150৳", base:150, image:"djbub.jpg" },
+{ name:"RGB DJ BULB", price:"160৳", base:160, image:"rgbdj.jpg" },
+{ name:"LED MUSIC BULB", price:"260৳", base:260, image:"musicbulb.jpg" },
 
-{
-name:"LED BULB 30W",
-price:"170৳",
-base:170,
-image:"ledbulb30w.jpg"
-},
+{ name:"DC LED SOLAR", price:"90-280৳", base:90, image:"dcsolar.jpg" },
 
-{
-name:"LED BULB 50W",
-price:"280-420৳",
-base:280,
-image:"ledbulb50w.jpg"
-}
+{ name:"MINI HAND FAN", price:"220-300৳", base:220, image:"minifan.jpg" },
+
+{ name:"CHARGER HARIKEN LIGHT", price:"85৳", base:85, image:"hariken.jpg" },
+
+{ name:"LICHI LED BULB", price:"220৳", base:220, image:"lichi.jpg" },
+
+{ name:"SOLDARING IRON", price:"175৳", base:175, image:"solderiron.jpg" },
+{ name:"SOLDER", price:"130৳", base:130, image:"solder.jpg" },
+
+{ name:"PIN CAP", price:"6৳", base:6, image:"pincap.jpg" },
+
+{ name:"AC CIRCUIT", price:"15-25৳", base:15, image:"accircuit.jpg" },
+{ name:"DC 4V PC", price:"17-20৳", base:17, image:"dc4v.jpg" },
+
+{ name:"AC/DC PC", price:"70৳", base:70, image:"acdcpc.jpg" },
+{ name:"AC 20W PC", price:"20৳", base:20, image:"ac20w.jpg" },
+{ name:"AC 50W PC", price:"130৳", base:130, image:"ac50w.jpg" },
+
+{ name:"4 VOLT MOTOR", price:"13-25৳", base:13, image:"4vmotor.jpg" },
+
+{ name:"1.2 RECHARGEABLE BATTERY", price:"90৳", base:90, image:"battery12.jpg" },
+
+{ name:"WEIGHT SCALE BATTERY", price:"275-520৳", base:275, image:"weightbattery.jpg" },
+
+{ name:"MEGHAN WEIGHT SCALE", price:"2800-3600৳", base:2800, image:"meghan.jpg" },
+{ name:"MEGA WEIGHT SCALE", price:"2500-3500৳", base:2500, image:"mega.jpg" },
+
+{ name:"LED FLOOD LIGHT 10W", price:"550-750৳", base:550, image:"flood10w.jpg" },
+{ name:"LED FLOOD LIGHT 20W", price:"750-850৳", base:750, image:"flood20w.jpg" },
+{ name:"LED FLOOD LIGHT 30W", price:"700-900৳", base:700, image:"flood30w.jpg" },
+{ name:"LED FLOOD LIGHT 50W", price:"1050-1850৳", base:1050, image:"flood50w.jpg" },
+{ name:"LED FLOOD LIGHT 100W", price:"1800-2600৳", base:1800, image:"flood100w.jpg" },
+{ name:"LED FLOOD LIGHT 150W", price:"3200-3500৳", base:3200, image:"flood150w.jpg" },
+{ name:"LED FLOOD LIGHT 200W", price:"5000-5500৳", base:5000, image:"flood200w.jpg" },
+
+{ name:"LED SPOT LIGHT 3W", price:"260-320৳", base:260, image:"spot3w.jpg" },
+
+{ name:"LED PANEL LIGHT 6W", price:"550-650৳", base:550, image:"panel6w.jpg" },
+{ name:"LED PANEL LIGHT 12W", price:"660-900৳", base:660, image:"panel12w.jpg" },
+
+{ name:"LED CANDLE LIGHT", price:"90৳", base:90, image:"candle.jpg" },
+
+{ name:"CIRCUIT BREAKER 1 POLE", price:"160৳", base:160, image:"cb1.jpg" },
+{ name:"CIRCUIT BREAKER 2 POLE", price:"280৳", base:280, image:"cb2.jpg" },
+{ name:"CIRCUIT BREAKER 3 POLE", price:"420৳", base:420, image:"cb3.jpg" },
+
+{ name:"SUPER SWITCH", price:"160৳", base:160, image:"superswitch.jpg" },
+{ name:"SUPER SOCKET", price:"160৳", base:160, image:"supersocket.jpg" },
+{ name:"SUPER INDICATOR", price:"160৳", base:160, image:"superindicator.jpg" },
+{ name:"SUPER FUSE", price:"185৳", base:185, image:"superfuse.jpg" },
+{ name:"SUPER DIMMER", price:"60৳", base:60, image:"superdimmer.jpg" }
 
 ]
